4bd5eb6742498ad4d9939a5ec7e1de3a3eb03c0b
zavalinsky <zavalinsky@gmail.com>
2026-05-18 15:37:20 +0400

    bugfix: adding component error

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
 import tkinter as tk
 from tkinter import ttk
-
 from tabs.materials_tab import create_materials_tab
+from tabs.components_tab import create_components_tab
+from tabs.techprocess_tab import create_techprocess_tab
 from tabs.products_tab import create_products_tab
 from tabs.reports_tab import create_reports_tab
-from tabs.components_tab import create_components_tab
-
 
 root = tk.Tk()
 root.title("Мебельное производство")
 root.geometry("1400x800")
 
-
 notebook = ttk.Notebook(root)
 notebook.pack(fill="both", expand=True)
 
-
 create_materials_tab(notebook)
 create_components_tab(notebook)
+create_techprocess_tab(notebook)
 create_products_tab(notebook)
 create_reports_tab(notebook)
-
 
 root.mainloop()
